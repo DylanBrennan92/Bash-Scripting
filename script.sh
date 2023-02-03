2a622f59adb25f1b56cf7ca6f940f948e9c21c89
DylanBrennan92 <x18119859@student.ncirl.ie>
2023-02-03 14:47:41 +0000

    Modified file permissions and created README.md

--- script.sh
+++ script.sh
@@ -7,12 +7,17 @@ do
     do
     filename="$x-$i.txt"
     touch "$filename"
+        if [[ $i -eq 1 ]] || [[ $i -eq 3 ]]; then
+            chmod o+rw "$filename"
+            echo "rw changed for _1 or _3"  
+        elif [ $i -eq 2 ]; then
+            chmod ugo+rwx "$filename"
+            echo "rwx changed for _2"     
+        fi
     done
     cd ..
-    rm -rf folder"$x"
-    echo "deleted folder"$x
 done
 
 
 
-echo "Folders & files created and then deleted"
+echo "Folders & files created and file permissions edited for files ending in _1 and _3"
